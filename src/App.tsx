2d667740870d6e01c1edf31a54a88f9c8c591ffb
moqada <moqada@gmail.com>
2024-12-24 08:52:44 +0900

    feat: Implement minimum UI
    - いったんデフォルトのスタイルをほぼそのままに必要最低限の要素を追加した
    - デバイス名と GIF を指定、設定を保存できる

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,127 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import React, {useCallback, useEffect, useState} from 'react';
+import {invoke} from '@tauri-apps/api/core';
+import './App.css';
+
+const GIF_FILE_TYPES = ['id', 'url'] as const;
+
+type AppSettings = {
+  gifFileId: string;
+  gifFileUrl: string;
+  gifFileType: (typeof GIF_FILE_TYPES)[number];
+  targetDeviceName: string;
+};
+
+const loadSettings = async (): Promise<AppSettings> => {
+  const settings: AppSettings = await invoke('load_settings');
+  return settings;
+};
+
+const saveSettings = async (settings: AppSettings): Promise<void> => {
+  await invoke('save_settings', settings);
+};
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  const [targetDeviceName, setTargetDeviceName] = useState('');
+  const [gifFileId, setGifFileId] = useState('');
+  const [gifFileUrl, setGifFileUrl] = useState('');
+  const [gifFileType, setGifFileType] = useState<'id' | 'url'>('id');
+  const onChangeTargetDeviceName = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTargetDeviceName(e.currentTarget.value);
+    },
+    []
+  );
+  const onChangeGifFileId = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setGifFileId(e.currentTarget.value);
+    },
+    []
+  );
+  const onChangeGifFileUrl = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setGifFileUrl(e.currentTarget.value);
+    },
+    []
+  );
+  const onChangeGifFileType = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setGifFileType(e.currentTarget.value as 'id' | 'url');
+    },
+    []
+  );
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      saveSettings({
+        gifFileId,
+        gifFileType,
+        gifFileUrl,
+        targetDeviceName,
+      });
+    },
+    [gifFileId, gifFileType, gifFileUrl, targetDeviceName]
+  );
+  useEffect(() => {
+    loadSettings().then((settings) => {
+      setTargetDeviceName(settings.targetDeviceName);
+      setGifFileId(settings.gifFileId);
+      setGifFileUrl(settings.gifFileUrl);
+      setGifFileType(settings.gifFileType);
+    });
+  }, []);
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
+      <form className="row" onSubmit={onSubmit}>
+        <label htmlFor="input-target-device-name">Target Device Name</label>
         <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          id="input-target-device-name"
+          onChange={onChangeTargetDeviceName}
+          defaultValue={targetDeviceName}
+          placeholder="Enter target device name..."
         />
-        <button type="submit">Greet</button>
+        <fieldset>
+          <legend>GIF File Type</legend>
+          {GIF_FILE_TYPES.map((type) => (
+            <React.Fragment key={type}>
+              <input
+                type="radio"
+                name="gif-type"
+                id={`input-gif-type-${type}`}
+                onChange={onChangeGifFileType}
+                checked={gifFileType === type}
+                value={type}
+              />
+              <label htmlFor={`input-gif-type-${type}`}>
+                {type.toUpperCase()}
+              </label>
+            </React.Fragment>
+          ))}
+        </fieldset>
+        {gifFileType === 'id' && (
+          <>
+            <label htmlFor="input-gif-file-id">GIF File ID</label>
+            <input
+              id="input-gif-file-id"
+              onChange={onChangeGifFileId}
+              defaultValue={gifFileId}
+              placeholder="Enter GIF File ID..."
+            />
+          </>
+        )}
+        {gifFileType === 'url' && (
+          <>
+            <label htmlFor="input-gif-file-url">GIF File URL</label>
+            <input
+              id="input-file-url"
+              onChange={onChangeGifFileUrl}
+              defaultValue={gifFileUrl}
+              placeholder="Enter GIF File URL..."
+            />
+          </>
+        )}
+        <button type="submit">Save</button>
       </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
